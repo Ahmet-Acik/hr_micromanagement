c1f237a22c7bfa9542c95d2701ea54506c94a537
drahmetacik <a.acik@icloud.com>
2025-04-08 11:36:44 +0100

    add Job class to represent job entities

---
+++ src/main/java/org/example/Job.java
@@ -0,0 +1,42 @@
+// Job.java
+package org.example;
+
+public class Job {
+    private int id;
+    private String title;
+    private double minSalary;
+    private double maxSalary;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(double minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public double getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(double maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+}
