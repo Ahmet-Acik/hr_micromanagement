6fd14058b1decaf6972678c0c4b14391a01241b6
drahmetacik <a.acik@icloud.com>
2025-04-08 11:37:19 +0100

    add Project class to represent project entities

---
+++ src/main/java/org/example/Project.java
@@ -0,0 +1,53 @@
+// Project.java
+package org.example;
+
+import java.util.Date;
+
+public class Project {
+    private int id;
+    private String name;
+    private Date startDate;
+    private Date endDate;
+    private double budget;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+}
