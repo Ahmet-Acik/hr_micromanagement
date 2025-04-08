adb535392f230fcc9d29cb432b10f5961bf06d64
drahmetacik <a.acik@icloud.com>
2025-04-08 11:33:38 +0100

    add DatabaseUtil class for database connection management

---
+++ src/main/java/org/example/DatabaseUtil.java
@@ -0,0 +1,16 @@
+// src/main/java/org/example/DatabaseUtil.java
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/hr_micromanagement";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
